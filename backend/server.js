cf980b5dde6951647a58d949e07e7214e86ef4ff
efsaneabbas <tu7k3zlya@code.edu.az>
2024-05-22 18:37:19 +0400

    second commir

--- backend/server.js
+++ backend/server.js
@@ -1,66 +1,66 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const cors = require("cors")
-require('dotenv').config()
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-mongoose.connect(process.env.CONNECTION__STRING)
-.then((resp)=>console.log("db connected"))
-.catch((err)=>console.log("db dont connected"))
+mongoose
+  .connect(process.env.CONNECTION__STRING)
+  .then((resp) => console.log("db connected"))
+  .catch((err) => console.log("db dont connected"));
 
-app.listen(process.env.PORT,(res=>{
-    console.log("Port Connected");
-}))
+app.listen(process.env.PORT, (res) => {
+  console.log("Port Connected");
+});
 
 const productShema = mongoose.Schema({
-    title:String,
-    img:String,
-    desc:String,
-    price:Number,
-    count:Number,
-    totalPrice:Number,
-})
+  title: String,
+  img: String,
+  desc: String,
+  price: Number,
+  count: Number,
+  totalPrice: Number,
+});
 
-const Products = mongoose.model("ExamFour",productShema)
+const Products = mongoose.model("ExamFour", productShema);
 
-app.get("/api/examfour",async (req,res)=>{
-    const resp = await Products.find()
-    res.send(resp)
-})
+app.get("/api/examfour", async (req, res) => {
+  const resp = await Products.find();
+  res.send(resp);
+});
 
-app.get("/api/examfour/:id",async (req,res)=>{
-    const {id} = req.params
-    const resp = await Products.findById(id)
-    res.send(resp)
-})
+app.get("/api/examfour/:id", async (req, res) => {
+  const { id } = req.params;
+  const resp = await Products.findById(id);
+  res.send(resp);
+});
 
-app.put("/api/examfour/:id",async (req,res)=>{
-    const {id} = req.params
-    await Products.findByIdAndUpdate(id,{...req.body})
-    res.send("item updated")
-})
+app.put("/api/examfour/:id", async (req, res) => {
+  const { id } = req.params;
+  await Products.findByIdAndUpdate(id, { ...req.body });
+  res.send("item updated");
+});
 
-app.delete("/api/examfour/:id",async (req,res)=>{
-    const {id} = req.params
-    const resp = await Products.findByIdAndDelete(id)
-    res.send("itemd deleted")
-})
+app.delete("/api/examfour/:id", async (req, res) => {
+  const { id } = req.params;
+  const resp = await Products.findByIdAndDelete(id);
+  res.send("itemd deleted");
+});
 
-app.post("/api/examfour",async (req,res)=>{
-    const {title,img,desc,price,count,totalPrice} = req.body
-    const newProduct = new Products({
-        title:title,
-        img:img,
-        desc:desc,
-        price:price,
-        count:count,
-        totalPrice:totalPrice
-    })
-    await newProduct.save()
-    res.send("new item created")
-})
-
+app.post("/api/examfour", async (req, res) => {
+  const { title, img, desc, price, count, totalPrice } = req.body;
+  const newProduct = new Products({
+    title: title,
+    img: img,
+    desc: desc,
+    price: price,
+    count: count,
+    totalPrice: totalPrice,
+  });
+  await newProduct.save();
+  res.send("new item created");
+});
